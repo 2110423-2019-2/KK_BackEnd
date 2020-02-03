55091044c3ce0a555e319d7aedb60324ed7c1203
Chutchatut Sutichavengkul <chutchatut@gmail.com>
2020-02-03 10:51:43 +0700

    update Serializers for user

--- api/serializers.py
+++ api/serializers.py
@@ -19,8 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name',
-                  'email', 'is_staff', 'reviews', 'documents',)
+        fields = ('username', 'first_name', 'last_name',)
 
 
 class UserLogSerializer(serializers.ModelSerializer):
@@ -31,13 +30,42 @@ class UserLogSerializer(serializers.ModelSerializer):
         fields = ('username', 'logs',)
 
 
-class ExtendedUserSerializer(serializers.ModelSerializer):
-    base_user = UserSerializer(many=False)
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = ('url', 'timestamp',)
+
+
+class ExtendedUserSerializer(serializers.HyperlinkedModelSerializer):
     ban_list = UserSerializer(many=True)
+    username = serializers.CharField(
+        source='base_user.username'
+    )
+    first_name = serializers.CharField(
+        source='base_user.first_name'
+    )
+    last_name = serializers.CharField(
+        source='base_user.last_name'
+    )
+    email = serializers.CharField(
+        source='base_user.email'
+    )
+    is_staff = serializers.BooleanField(
+        source='base_user.is_staff'
+    )
+    reviews = ReviewSerializer(
+        source='base_user.reviews', many=True
+    )
+    documents = DocumentSerializer(
+        source='base_user.documents', many=True
+    )
 
     class Meta:
         model = ExtendedUser
-        fields = ('base_user', 'ban_list', 'is_verified', 'phone_number', 'credit',)
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'ban_list', 'is_verified',
+                  'phone_number', 'credit', 'is_staff',
+                  'reviews', 'documents', )
 
 
 class RacketSerializer(serializers.ModelSerializer):
@@ -66,12 +94,6 @@ class CourtSerializer(serializers.ModelSerializer):
         model = Court
         fields = ('id', 'name', 'price', 'owner', 'desc',
                   'rating_count', 'avg_score', 'images',)
-
-
-class DocumentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Document
-        fields = ('url', 'timestamp',)
 
 
 class UserDocumentSerializer(serializers.ModelSerializer):
